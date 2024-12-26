3251fcb100c9c23a60652d519f480f8bda8fa3a0
jovinull <felipejovinogamerplay@gmail.com>
2024-12-26 12:16:29 -0300

    FEAT: Buscando comentários do post

--- src/pages/task/[id].tsx
+++ src/pages/task/[id].tsx
@@ -4,126 +4,164 @@ import { GetServerSideProps } from "next";
 import Head from "next/head";
 import styles from "./styles.module.css";
 
+import { Textarea } from "@/components/textarea";
+
 import { db } from "../../services/firebaseConnection";
 import {
-  doc,
-  collection,
-  query,
-  where,
-  getDoc,
-  addDoc,
+    doc,
+    collection,
+    query,
+    where,
+    getDoc,
+    addDoc,
+    getDocs,
 } from "firebase/firestore";
 
-import { Textarea } from "../../components/textarea";
-
 interface TaskProps {
-  item: {
-    tarefa: string;
-    created: string;
-    public: boolean;
-    user: string;
-    taskId: string;
-  };
+    item: {
+        tarefa: string;
+        created: string;
+        public: boolean;
+        user: string;
+        taskId: string;
+    };
+    allComments: CommentProps[];
 }
 
-export default function Task({ item }: TaskProps) {
-  const { data: session } = useSession();
+interface CommentProps {
+    id: string;
+    comment: string;
+    taskId: string;
+    user: string;
+    name: string;
+}
 
-  const [input, setInput] = useState("");
+export default function Task({ item, allComments }: TaskProps) {
+    const { data: session } = useSession();
 
-  async function handleComment(event: FormEvent) {
-    event.preventDefault();
+    const [input, setInput] = useState("");
+    const [comments, setComments] = useState<CommentProps[]>(allComments || []);
 
-    if (input === "") return;
+    async function handleComment(event: FormEvent) {
+        event.preventDefault();
 
-    if (!session?.user?.email || !session?.user?.name) return;
+        if (input === "") return;
 
-    try {
-      const docRef = await addDoc(collection(db, "comments"), {
-        comment: input,
-        created: new Date(),
-        user: session?.user?.email,
-        name: session?.user?.name,
-        taskId: item?.taskId,
-      });
+        if (!session?.user?.email || !session?.user?.name) return;
 
-      setInput("");
-    } catch (err) {
-      console.log(err);
+        try {
+            const docRef = await addDoc(collection(db, "comments"), {
+                comment: input,
+                created: new Date(),
+                user: session?.user?.email,
+                name: session?.user?.name,
+                taskId: item?.taskId,
+            });
+
+            setInput("");
+        } catch (err) {
+            console.log(err);
+        }
     }
-  }
 
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Tarefa - Detalhes da tarefa</title>
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Tarefa - Detalhes da tarefa</title>
+            </Head>
 
-      <main className={styles.main}>
-        <h1>Tarefa</h1>
-        <article className={styles.task}>
-          <p>{item.tarefa}</p>
-        </article>
-      </main>
+            <main className={styles.main}>
+                <h1>Tarefa</h1>
+                <article className={styles.task}>
+                    <p>{item.tarefa}</p>
+                </article>
+            </main>
 
-      <section className={styles.commentsContainer}>
-        <h2>Deixar comentário</h2>
+            <section className={styles.commentsContainer}>
+                <h2>Deixar comentário</h2>
 
-        <form onSubmit={handleComment}>
-          <Textarea
-            value={input}
-            onChange={(event: ChangeEvent<HTMLTextAreaElement>) =>
-              setInput(event.target.value)
-            }
-            placeholder="Digite seu comentário..."
-          />
-          <button disabled={!session?.user} className={styles.button}>
-            Enviar comentário
-          </button>
-        </form>
-      </section>
-    </div>
-  );
+                <form onSubmit={handleComment}>
+                    <Textarea
+                        value={input}
+                        onChange={(event: ChangeEvent<HTMLTextAreaElement>) =>
+                            setInput(event.target.value)
+                        }
+                        placeholder="Digite seu comentário..."
+                    />
+                    <button disabled={!session?.user} className={styles.button}>
+                        Enviar comentário
+                    </button>
+                </form>
+            </section>
+
+            <section className={styles.commentsContainer}>
+                <h2>Todos comentários</h2>
+                {comments.length === 0 && (
+                    <span>Nenhum comentário foi encontrado...</span>
+                )}
+
+                {comments.map((item) => (
+                    <article key={item.id} className={styles.comment}>
+                        <p>{item.comment}</p>
+                    </article>
+                ))}
+            </section>
+        </div>
+    );
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  const id = params?.id as string;
+    const id = params?.id as string;
+    const docRef = doc(db, "tarefas", id);
 
-  const docRef = doc(db, "tarefas", id);
+    const q = query(collection(db, "comments"), where("taskId", "==", id));
+    const snapshotComments = await getDocs(q);
 
-  const snapshot = await getDoc(docRef);
+    let allComments: CommentProps[] = [];
+    snapshotComments.forEach((doc) => {
+        allComments.push({
+            id: doc.id,
+            comment: doc.data().comment,
+            user: doc.data().user,
+            name: doc.data().name,
+            taskId: doc.data().taskId,
+        });
+    });
 
-  if (snapshot.data() === undefined) {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
+    const snapshot = await getDoc(docRef);
+
+    if (snapshot.data() === undefined) {
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        };
+    }
+
+    if (!snapshot.data()?.public) {
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        };
+    }
+
+    const miliseconds = snapshot.data()?.created?.seconds * 1000;
+
+    const task = {
+        tarefa: snapshot.data()?.tarefa,
+        public: snapshot.data()?.public,
+        created: new Date(miliseconds).toLocaleDateString(),
+        user: snapshot.data()?.user,
+        taskId: id,
     };
-  }
 
-  if (!snapshot.data()?.public) {
     return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
+        props: {
+            item: task,
+            allComments: allComments,
+        },
     };
-  }
-
-  const miliseconds = snapshot.data()?.created?.seconds * 1000;
-
-  const task = {
-    tarefa: snapshot.data()?.tarefa,
-    public: snapshot.data()?.public,
-    created: new Date(miliseconds).toLocaleDateString(),
-    user: snapshot.data()?.user,
-    taskId: id,
-  };
-
-  return {
-    props: {
-      item: task,
-    },
-  };
 };
