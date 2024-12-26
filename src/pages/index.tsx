e4ce0af2434bd48c2cdcfcc336cb26a03f52229d
jovinull <felipejovinogamerplay@gmail.com>
2024-12-26 15:15:16 -0300

    FEAT: Mostrando quantidade de posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,24 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css"
 import Image from "next/image";
 
-import heroImg from "../../public/assets/hero.png"
+import heroImg from "../../public/assets/hero.png";
 
-export default function Home() {
+import { db } from "../services/firebaseConnection";
+
+import { collection, getDocs } from "firebase/firestore";
+
+interface HomeProps {
+  posts: number;
+  comments: number;
+}
+
+export default function Home({ posts, comments }: HomeProps) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Tarefas + | Organize suas tarefas de forma fácil</title>
+        <title>Tarefas+ | Organize suas tarefas de forma fácil</title>
       </Head>
 
       <main className={styles.main}>
@@ -21,19 +31,35 @@ export default function Home() {
           />
         </div>
         <h1 className={styles.title}>
-          Sistema feito para você organizar <br/>
-          seus estudos e tarefas
+          Sistema feito para você organizar <br />
+          seus estudos e terefas
         </h1>
 
         <div className={styles.infoContent}>
           <section className={styles.box}>
-            <span>+12 posts</span>
+            <span>+{posts} posts</span>
           </section>
           <section className={styles.box}>
-            <span>+90 comentários</span>
+            <span>+{comments} comentários</span>
           </section>
         </div>
       </main>
     </div>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const commentRef = collection(db, "comments");
+  const postRef = collection(db, "tarefas");
+
+  const commentSnapshot = await getDocs(commentRef);
+  const postSnapshot = await getDocs(postRef);
+
+  return {
+    props: {
+      posts: postSnapshot.size || 0,
+      comments: commentSnapshot.size || 0,
+    },
+    revalidate: 60, // seria revalidada a cada 60 segundos
+  };
+};
