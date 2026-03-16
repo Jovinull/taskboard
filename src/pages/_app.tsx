04b56d378808b237da7088fb38bf0bc90d6cc992
jovinull <felipejovinogamerplay@gmail.com>
2026-03-16 20:32:02 -0300

    refactor: atualizar arquivos de configuracao Next.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
-import { Header } from "@/components/header";
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { Header } from "@/components/header";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
-      <Header/>
+      <Header />
       <Component {...pageProps} />
     </SessionProvider>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-BR">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="description" content="Organize suas tarefas e estudos de forma simples e colaborativa." />
+      </Head>
       <body>
         <Main />
         <NextScript />
