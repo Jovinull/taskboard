cfe70fded65a9043c5ae303d10035f41987440ae
jovinull <felipejovinogamerplay@gmail.com>
2026-03-16 20:32:41 -0300

    security: mover credenciais Firebase para variaveis de ambiente

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -1,17 +1,15 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAckNTe9BvSlnT8zXZJK5uU2pvPLxNOQ0g",
-  authDomain: "taskboarddb.firebaseapp.com",
-  projectId: "taskboarddb",
-  storageBucket: "taskboarddb.firebasestorage.app",
-  messagingSenderId: "667889492478",
-  appId: "1:667889492478:web:de59e935bf60d2d2dfe0af"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-const db = getFirestore(firebaseApp)
-
-export { db };
+export const db = getFirestore(firebaseApp);
