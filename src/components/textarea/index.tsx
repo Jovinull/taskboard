20b238e39cd31db0bd54040c8ab06d7432b479c9
jovinull <felipejovinogamerplay@gmail.com>
2026-03-16 20:32:00 -0300

    refactor: simplificar componente Textarea

--- src/components/textarea/index.tsx
+++ src/components/textarea/index.tsx
@@ -1,13 +1,13 @@
 import { TextareaHTMLAttributes } from "react";
 import styles from "./styles.module.css";
 
-interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {}
+type TextareaProps = TextareaHTMLAttributes<HTMLTextAreaElement>;
 
 export function Textarea({ className, ...rest }: TextareaProps) {
-    return (
-        <textarea
-            className={`${styles.textarea} ${className || ""}`.trim()}
-            {...rest}
-        ></textarea>
-    );
+  return (
+    <textarea
+      className={`${styles.textarea} ${className || ""}`.trim()}
+      {...rest}
+    />
+  );
 }
